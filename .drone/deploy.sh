9949547cd6e9c692099c889f8ea8d22e664f4265
Fabio Parra <faparraf@gmail.com>
2018-07-25 11:49:36 -0500

    Eliminación de llave

--- .drone/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-docker stop ${DRONE_REPO##udistrital/} || true
-docker rm ${DRONE_REPO##udistrital/} || true
-docker pull oas0/${DRONE_REPO##udistrital/}:${DRONE_COMMIT:0:7}
-docker run --name ${DRONE_REPO##udistrital/} oas0/${DRONE_REPO##udistrital/}:${DRONE_COMMIT:0:7}
-
